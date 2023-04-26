998d1928c6eb68b84eee12e3523ab66d35a3da7c
evgeniy.astafyev <evgeniy.astafyev@raftds.com>
2023-04-26 10:30:17 +0300

    FluentValidation

--- Notes.Backend/Notes.Application/DependencyInjection.cs
+++ Notes.Backend/Notes.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Common.Behaviors;
 using Notes.Application.Interfaces;
 
 namespace Notes.Application
@@ -10,6 +12,8 @@ namespace Notes.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+            services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
             return services;
         }
